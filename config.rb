7676ab6fa341507d53ab4232d7d2544b7f31b89c
rhidja <rhidja@gmail.com>
2017-02-17 23:55:01 +0100

    Restructuration des assets suite

--- config.rb
+++ config.rb
@@ -3,11 +3,11 @@ require 'compass/import-once/activate'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "css"
+css_dir = "assets/css"
 sass_dir = "sass"
-images_dir = "img"
-javascripts_dir = "js"
-http_generated_images_path = "../img"
+images_dir = "assets/img"
+javascripts_dir = "assets/js"
+http_generated_images_path = "../assets/img"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
